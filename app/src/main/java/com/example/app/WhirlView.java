86f52b46ab7e03fd1306d2ebb2f2362d58f8ccf4
Ilya Peresadin <pva701@gmail.com>
2014-09-16 20:57:38 +0400

    Draw and Update in differente threads. Used CyclicBarrier. Shit with static for local thread variables.

--- app/src/main/java/com/example/app/WhirlView.java
+++ app/src/main/java/com/example/app/WhirlView.java
@@ -7,28 +7,29 @@ import android.graphics.RectF;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import java.util.Random;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 /**
 * Created by thevery on 11/09/14.
 */
 
 class WhirlView extends SurfaceView implements Runnable {
-    int[][] field = null;
-    int[] drawingField;
-    Bitmap buffer;
-    int width, height;
-    RectF screenRect;
-    final int MAX_COLOR = 10;
-    int[] palette = {0xFFFF0000, 0xFF800000, 0xFF808000, 0xFF008000, 0xFF00FF00, 0xFF008080, 0xFF0000FF, 0xFF000080, 0xFF800080, 0xFFFFFFFF};
-    SurfaceHolder holder;
-    Thread thread = null;
-    volatile boolean running = false;
-    volatile boolean fieldStabilized = false;
+    private int[] drawingField;
+    private Bitmap buffer;
+    private int width, height;
+    private RectF screenRect;
+    private UpdateThread updater;
+    private SurfaceHolder holder;
+    private Thread thread = null;
+    private volatile boolean running = false;
+    private volatile CyclicBarrier barrier;
 
     public WhirlView(Context context) {
         super(context);
         holder = getHolder();
+        barrier = new CyclicBarrier(2);
         setDrawingCacheQuality(DRAWING_CACHE_QUALITY_HIGH);
     }
 
@@ -40,8 +41,12 @@ class WhirlView extends SurfaceView implements Runnable {
 
     public void pause() {
         running = false;
+        barrier.reset();
+        barrier = new CyclicBarrier(1);
+        updater.stopRunning();
         try {
             thread.join();
+            updater.join();
         } catch (InterruptedException ignore) {}
     }
 
@@ -51,8 +56,13 @@ class WhirlView extends SurfaceView implements Runnable {
             if (holder.getSurface().isValid()) {
                 long startTime = System.nanoTime();
                 Canvas canvas = holder.lockCanvas();
-                updateField();
+                drawingField = updater.lockField();
+                updater.update();
                 draw(canvas);
+                try {
+                    barrier.await();
+                } catch (BrokenBarrierException ignore) {}
+                catch (InterruptedException e) {}
                 holder.unlockCanvasAndPost(canvas);
                 long finishTime = System.nanoTime();
                 ++tot;
@@ -62,6 +72,7 @@ class WhirlView extends SurfaceView implements Runnable {
                     timeSum = 0;
                     tot = 0;
                 }
+                //return;
             }
         }
     }
@@ -76,70 +87,16 @@ class WhirlView extends SurfaceView implements Runnable {
             height = 240;
         }
         buffer = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        initField();
+        if (updater != null)
+            updater.interrupt();
+        updater = new UpdateThread(width, height, barrier);
+        updater.start();
         screenRect = new RectF(0, 0, w, h);
-    }
-
-    void initField() {
-        drawingField = new int[width * height];
-        field = new int[width][height];
-        Random rand = new Random();
-        for (int x = 0; x < width; x++)
-            for (int y = 0; y < height; y++) {
-                field[x][y] = rand.nextInt(MAX_COLOR);
-                drawingField[y * width + x] = palette[field[x][y]];
-            }
-    }
-
-    private void incCells() {
-        for (int i = 0; i < width; ++i)
-            for (int j = 0; j < height; ++j) {
-                field[i][j]++;
-                if (field[i][j] == MAX_COLOR)
-                    field[i][j] = 0;
-                drawingField[j * width + i] = palette[field[i][j]];
-            }
-    }
-
-    void updateField() {
-        if (fieldStabilized) {
-            incCells();
-            return;
-        }
-
-        int[][] field2 = new int[width][height];
-        int founds = 0;
-        for (int x=0; x< width; x++) {
-            for (int y=0; y< height; y++) {
-                field2[x][y] = field[x][y];
-                int newColor = (field[x][y] + 1) % MAX_COLOR;
-                boolean found = false;
-                for (int dx = -1; dx <= 1 && !found; dx++) {
-                    for (int dy = -1; dy <= 1 && !found; dy++) {
-                        int x2 = x + dx;
-                        int y2 = y + dy;
-                        if (x2 < 0) x2 += width;
-                        if (y2 < 0) y2 += height;
-                        if (x2 >= width) x2 -= width;
-                        if (y2 >= height) y2 -= height;
-
-                        found = newColor == field[x2][y2];
-                    }
-                }
-
-                if (found) {
-                    field2[x][y] = newColor;
-                    ++founds;
-                    drawingField[y * width + x] = palette[field2[x][y]];
-                }
-            }
-        }
-        field = field2;
-        fieldStabilized = founds == width * height;
     }
 
     @Override
     public void draw(Canvas canvas) {
+        Log.i("PIXEL", "COL" + drawingField[0] + " " + drawingField[1]);
         buffer.setPixels(drawingField, 0, width, 0, 0, width, height);
         canvas.drawBitmap(buffer, null, screenRect, null);
     }
